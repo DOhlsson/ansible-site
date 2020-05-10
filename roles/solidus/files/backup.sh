df45c60eabde43ece94bb3f097429aec182f79a7
Daniel Ohlsson <dohlsson89@gmail.com>
2020-05-10 14:26:38 +0200

    Added trbg backups

---
+++ roles/solidus/files/backup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+backup_dir=/tank/trbg
+date=$(date +%Y-%m-%d)
+
+# to enable the much slower rsync checksum for file difference calculation
+if [[ "$1" == "--checksum" ]]; then
+  checksum_flag=$1
+fi
+
+rsync -rv $checksum_flag --delete pallastv:/mnt/hdd/Data/ ${backup_dir}/data/ > ${backup_dir}/rsync_stdout.log 2> ${backup_dir}/rsync_stderr.log
+
+zfs snapshot tank/trbg@backup-$date
