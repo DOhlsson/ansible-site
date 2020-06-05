c4a696906c650618cf2e62fe3e7fd4d300f4e028
Daniel Ohlsson <dohlsson89@gmail.com>
2020-06-05 21:13:09 +0200

    rsync in archive mode

--- roles/solidus/files/backup.sh
+++ roles/solidus/files/backup.sh
@@ -8,6 +8,6 @@ if [[ "$1" == "--checksum" ]]; then
   checksum_flag=$1
 fi
 
-rsync -rv $checksum_flag --delete pallastv:/mnt/hdd/Data/ ${backup_dir}/data/ > ${backup_dir}/rsync_stdout.log 2> ${backup_dir}/rsync_stderr.log
+rsync -av $checksum_flag --delete pallastv:/mnt/hdd/Data/ ${backup_dir}/data/ > ${backup_dir}/rsync_stdout.log 2> ${backup_dir}/rsync_stderr.log
 
 /sbin/zfs snapshot tank/trbg@backup-$date
