1924429ecf7e0ef6ab8f8ebf5c8cac6dc17472ad
Daniel Ohlsson <dohlsson89@gmail.com>
2021-02-11 20:51:36 +0100

    Added worky and fixed bugs

--- bootstrap-host.sh
+++ bootstrap-host.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 
-# make this into a playbook instead, should be possible
-
-# install python & sudo
-# setup do account
-# add to sudo group
-# add authorized_keys
+# TODO make this into a playbook instead
 
 if [ $# -ne 1 ]; then
 	echo "Usage: $0 <host>"
@@ -15,6 +10,6 @@ fi
 host=$1
 rootcmds="apt-get update && apt-get install -y python3 sudo && /sbin/usermod -a -G sudo do"
 
-ssh "do@$host" "su -c \"$rootcmds\""
-
 ssh-copy-id "do@$host"
+
+ssh "do@$host" "su -c \"$rootcmds\""
